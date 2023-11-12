c19171661464f77091f58e6a294e6715635ca125
Steven Pham <phamhaisteven@gmail.com>
2023-11-12 00:27:18 -0600

    Beginning front-end design

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import {
+  Table,
+  TableBody, 
+  TableCell, 
+  Box, 
+  Typography, 
+  TableRow, 
+  Paper, 
+  Checkbox, 
+  LinearProgress,
+  Toolbar } from '@mui/material' 
+import { makeStyles } from '@mui/styles';
+
+interface Row {
+  type: string;
+  description: string;
+  date: string;
+  selected: boolean;
+}
+
+function createData(type: string, description: string, date: string): Row {
+  return { type, description, date, selected: false };
+}
+
+const useStyles = makeStyles(({
+  root: {
+    width: '500px',
+  },
+  table: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  linearProgress: {
+    borderRadius: 5, 
+    height: 10, 
+    margin: 10  // Position LinearProgress at the bottom of the table row
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'lightblue',
+    borderRadius: '5px 5px 0px 0px',
+  },
+  title: {
+    top: 'auto',
+    bottom: 0,
+    textAlign: 'center', 
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
+  const [progress, setProgress] = React.useState<number>(0); //progess and setProgess with default of 0
+  const [rows, setRows] = React.useState<Row[]>([
+    createData('Math', 'Homework 3', '2020-09-10'),
+    createData('Reading', 'To Kill a Mocking Bird', '2020-05-21'),
+  ]); //rows and setRows with these being the default rows
+
+  useEffect(() => {
+    const savedRows = JSON.parse(localStorage.getItem('rows') || '[]');
+    const savedProgress = JSON.parse(localStorage.getItem('progress') || '0');
+
+    setRows(savedRows);
+    setProgress(savedProgress);
+  }, []); //useEffect which whenever refreshed you will getItems of the progess and rows then set them to the current state from the local storage 
+
+  const handleCheckboxChange = (index: number) => {
+    const updatedRows = [...rows];
+    updatedRows[index].selected = !updatedRows[index].selected;
+
+    // Save updated rows to localStorage
+    localStorage.setItem('rows', JSON.stringify(updatedRows));
+
+    // Calculate and save progress value to localStorage
+    const progressValue = calculateProgress(updatedRows);
+    localStorage.setItem('progress', JSON.stringify(progressValue));
+
+    setRows(updatedRows);
+    setProgress(progressValue); // Update progress state
+  };
+
+  const calculateProgress = (rows: Row[]) => {
+    return rows.length > 0 ? (rows.filter((r) => r.selected).length / rows.length) * 100 : 0;
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper className={classes.root}>
+      <Toolbar variant='dense' className={classes.toolbar}>
+        <Typography variant="h6" className={classes.title} sx={{ fontFamily: 'Monospace' }}> BE DESCIPLINED </Typography>
+      </Toolbar>
+        <Table className={classes.table}>
+          <TableBody>
+            {rows.map((row, index) => (
+              <TableRow
+                key={row.type}
+              >
+                <TableCell>
+                  <Checkbox
+                    checked={row.selected}
+                    onChange={() => handleCheckboxChange(index)}
+                  />
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.type}
+                </TableCell>
+                <TableCell align="center">{row.description}</TableCell>
+                <TableCell align="center">{row.date}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <Box>
+          <LinearProgress
+            variant="determinate"
+            value={progress}
+            className={classes.linearProgress}
+          />
+        </Box>
+        <Box>
+          <Typography variant="body2" color="text.secondary" sx={{margin: '10px'}}>{`${Math.round(
+            progress
+          )}%`}</Typography>
+        </Box>
+    </Paper>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.createElement("div");
+rootElement.id = "react-chrome-app";
+document.body.appendChild(rootElement);
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
